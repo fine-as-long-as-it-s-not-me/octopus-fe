cea90a06f1b50efb5774fce0659c8668731e61ed
wndgur2 <wndgur76@naver.com>
2026-02-02 15:49:52 +0900

    refactor: streamline socket handlers; remove unused code and improve structure

--- src/hooks/useSocketHandlers.ts
+++ src/hooks/useSocketHandlers.ts
@@ -1,18 +1,17 @@
 import { useTranslation } from 'react-i18next'
 
-import { SYSTEM } from '@/consts'
 import { useToast } from '@/context/ToastContext'
 import { useWindow } from '@/context/WindowContext'
+import { createChatHandlers } from '@/hooks/socketHandlers/chatHandlers'
 import { createErrorHandlers } from '@/hooks/socketHandlers/errorHandlers'
 import { createGameHandlers } from '@/hooks/socketHandlers/gameHandlers'
 import { createRoomHandlers } from '@/hooks/socketHandlers/roomHandlers'
 import { createRoundHandlers } from '@/hooks/socketHandlers/roundHandlers'
-import i18n from '@/i18n'
 import { useGameStore } from '@/store/gameStore'
 import { useRoomStore } from '@/store/roomStore'
 import { useRoundStore } from '@/store/roundStore'
 import { useUserStore } from '@/store/userStore'
-import type { ChatResponse, MessageHandlers, SystemChatResponse } from '@/types'
+import type { MessageHandlers } from '@/types'
 
 export function useSocketHandlers(
   sendMessage: (
@@ -24,121 +23,53 @@ export function useSocketHandlers(
   const { t } = useTranslation()
   const { notify } = useToast()
   const { isFullscreen, fullscreenToggle } = useWindow()
-  const { addChat, setRoomCode, setPlayers, setSettings, setCustomWords } =
-    useRoomStore()
 
-  const { name, UUID, setId, lang } = useUserStore()
+  const user = useUserStore()
+  const room = useRoomStore()
   const game = useGameStore()
-  const {
-    setPhase,
-    init,
-    setTied,
-    setGuessed,
-    setIsUnanimity,
-    setKeyword,
-    setStrokes,
-    setTimeLeft,
-    setOctopuses,
-    setVoteResult,
-    setCanvasColor,
-    setPainterUUID,
-    setNextPainterUUID,
-    setVotedPlayer,
-    setWinningTeam,
-  } = useRoundStore()
-
-  const addSystemChat = (text: string) => {
-    addChat({ player: SYSTEM, text })
-  }
-
-  console.warn('Socket handlers created', lang, i18n.language)
+  const round = useRoundStore()
 
   return {
     ...createRoomHandlers({
-      name: name,
-      UUID: UUID,
+      name: user.name,
+      UUID: user.UUID,
       sendMessage,
-      setId: setId,
-      setRoomCode: setRoomCode,
-      setPlayers: setPlayers,
-      setSettings: setSettings,
-      setPhase: setPhase,
+      setId: user.setId,
+      setRoomCode: room.setRoomCode,
+      setPlayers: room.setPlayers,
+      setSettings: room.setSettings,
+      setPhase: round.setPhase,
     }),
 
-    chat_added: ({ player, text }: ChatResponse) => {
-      const chat = { player, text }
-      addChat(chat)
-    },
-
-    system_chat: ({ type, variable }: SystemChatResponse) => {
-      switch (type) {
-        case 'player_joined': {
-          const text = `${(variable as { name: string }).name} ${t('joined the game')}.`
-          addSystemChat(text)
-          break
-        }
-        case 'player_left':
-          addSystemChat(
-            `${(variable as { name: string }).name} ${t('left the game')}.`,
-          )
-          break
-        case 'discussion_time_changed': {
-          const { name, amount } = variable as {
-            amount: number
-            name: string
-          }
-          addSystemChat(
-            `${name} ${t(`has ${amount > 0 ? 'extended' : 'shortened'} the remaining time`)}.`,
-          )
-          break
-        }
-        case 'player_voted': {
-          const { voterName } = variable as { voterName: string }
-          addSystemChat(`${voterName} ${t('has voted')}.`)
-          break
-        }
-        case 'revote': {
-          addSystemChat(
-            t(
-              'Revote has been initiated due to a tie. If tie occurs again, octopus wins.',
-            ),
-          )
-          break
-        }
-        case 'octopus_guessed': {
-          const { name, word } = variable as { name: string; word: string }
-          addSystemChat(`${name} ${t('guessed the code :')} '${word}'`)
-          break
-        }
-        default:
-          break
-      }
-    },
+    ...createChatHandlers({
+      t,
+      addChat: room.addChat,
+    }),
 
     ...createRoundHandlers({
-      initRound: init,
-      setTied: setTied,
-      setGuessed: setGuessed,
-      setIsUnanimity: setIsUnanimity,
-      setPhase: setPhase,
-      setKeyword: setKeyword,
-      setStrokes: setStrokes,
-      setTimeLeft: setTimeLeft,
-      setOctopuses: setOctopuses,
-      setVoteResult: setVoteResult,
-      setCanvasColor: setCanvasColor,
-      setPainterUUID: setPainterUUID,
-      setNextPainterUUID: setNextPainterUUID,
-      setVotedPlayer: setVotedPlayer,
-      setWinningTeam: setWinningTeam,
+      initRound: round.init,
+      setTied: round.setTied,
+      setGuessed: round.setGuessed,
+      setIsUnanimity: round.setIsUnanimity,
+      setPhase: round.setPhase,
+      setKeyword: round.setKeyword,
+      setStrokes: round.setStrokes,
+      setTimeLeft: round.setTimeLeft,
+      setOctopuses: round.setOctopuses,
+      setVoteResult: round.setVoteResult,
+      setCanvasColor: round.setCanvasColor,
+      setPainterUUID: round.setPainterUUID,
+      setNextPainterUUID: round.setNextPainterUUID,
+      setVotedPlayer: round.setVotedPlayer,
+      setWinningTeam: round.setWinningTeam,
       setRound: game.setRound,
       setRanks: game.setRanks,
-      setCustomWords: setCustomWords,
+      setCustomWords: room.setCustomWords,
     }),
 
     ...createGameHandlers({
       setRanks: game.setRanks,
-      addChat: addChat,
+      addChat: room.addChat,
       t,
       isFullscreen,
       fullscreenToggle,
