d396560703b395510ad8d887808326b4ff612c07
wndgur2 <wndgur76@naver.com>
2026-02-07 15:38:46 +0900

    feat: add player kicking functionality and update related components

--- src/components/player/PlayerListModal.tsx
+++ src/components/player/PlayerListModal.tsx
@@ -3,11 +3,15 @@ import Modal from '../common/Modal'
 import PlayerListItem from './PlayerListItem'
 
 export default function PlayerListModal() {
-  const { players } = useRoomStore()
+  const { players, hostUUID } = useRoomStore()
   return (
     <Modal>
       {players.map(player => (
-        <PlayerListItem key={player.UUID} {...player} />
+        <PlayerListItem
+          key={player.UUID}
+          {...player}
+          host={player.UUID === hostUUID}
+        />
       ))}
     </Modal>
   )
