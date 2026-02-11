8605cabe18a482a7c6c5769559903c95c979fcbb
wndgur2 <wndgur76@naver.com>
2026-02-11 16:56:04 +0900

    fix: UI overflow bug, reduce setting card size on mobile

--- src/components/room/RoomWrapper.tsx
+++ src/components/room/RoomWrapper.tsx
@@ -4,7 +4,10 @@ export default function RoomWrapper({
   children: React.ReactNode
 }) {
   return (
-    <div className='relative flex h-lvh w-full max-w-[1600px] flex-col overflow-hidden sm:gap-4 sm:p-4 md:p-8 lg:p-16'>
+    <div
+      id='room-wrapper'
+      className='relative flex h-full w-full max-w-[1600px] flex-col overflow-hidden sm:gap-4 sm:p-4 md:p-8 lg:p-16'
+    >
       {children}
     </div>
   )
